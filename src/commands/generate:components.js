e53e8d528c5f673ae9391ef4a91ae6c68f72bbef
Luis Reis <izohenrique@gmail.com>
2020-04-19 14:17:15 -0300

    updating
    Update docs
    
    Update error msg for invalid args

--- src/commands/generate:components.js
+++ src/commands/generate:components.js
@@ -7,6 +7,14 @@ module.exports = {
       createComponent,
     } = toolbox
 
+    if( parameters.array.length === 0){
+      print.error('Inavlid arguments!');
+
+      print.info('Name at least on component. Ex: generate:components component1 component2 ...');
+
+      return;
+    }
+
     for (var i = 0; i < parameters.array.length; i++) {
       const name = parameters.array[i]
 
--- src/commands/generate:pages.js
+++ src/commands/generate:pages.js
@@ -5,8 +5,17 @@ module.exports = {
     const {
       parameters,
       createComponent,
+      print,
     } = toolbox
 
+    if( parameters.array.length === 0){
+      print.error('Inavlid arguments!');
+
+      print.info('Name at least on page. Ex: generate:pages page1 page2 ...');
+
+      return;
+    }
+    
     for (var i = 0; i < parameters.array.length; i++) {
       const name = parameters.array[i]
 
